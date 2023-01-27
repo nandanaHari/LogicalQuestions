c9cc6feca56ed688ddea0ff42c80522d626241a9
nandanaHari <117264004+nandanaHari@users.noreply.github.com>
2023-01-27 13:13:46 +0530

    Add files via upload

---
+++ Question2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Enter the fare");
+float fare = float.Parse(Console.ReadLine());
+
+Console.WriteLine("\n Enter the time in 24 Hours Format");
+float time = float.Parse(Console.ReadLine());
+
+float newFare = 0;
+
+if (time > 6 & time < 9)
+{
+    newFare = fare + (fare * 10 / 100);
+    Console.WriteLine("\nfare = "+ newFare);
+}
+else if (time > 9 & time < 17)
+{
+    newFare = fare + (fare * 20 / 100);
+    Console.WriteLine("\nfare = "+ newFare);
+}
+else if (time > 17 & time < 23)
+{
+    newFare = fare + (fare * 7 / 100);
+    Console.WriteLine("\nfare = "+ newFare);
+}
+else if (time < 6)
+{
+    newFare = fare + (fare * 5 / 100);
+    Console.WriteLine("\nfare = "+ newFare);
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.WriteLine("Enter 10 Flight Fares");
+decimal[] fare = new decimal[4];
+
+for(int i = 0;i < 4; i++)
+{
+    Console.Write($"Enter Fare {i+1} : ");
+    fare[i] = decimal.Parse(Console.ReadLine());
+}
+
+Console.WriteLine();
+
+Console.Write("Ascending Order of Flight Fare : ");
+
+for(int i = 0;i < fare.Length; i++)
+{
+    for (int j = i+1; j < fare.Length; j++)
+    {
+        if (fare[i] > fare[j])
+        {
+            decimal temp = fare[i];
+            fare[i] = fare[j];
+            fare[j] = temp;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", fare));
+
+Console.WriteLine();
+
+Console.Write("Descenting Order of Flight Fare : ");
+
+for (int i = 0; i < fare.Length; i++)
+{
+    for (int j = i + 1; j < fare.Length; j++)
+    {
+        if (fare[i] < fare[j])
+        {
+            decimal temp = fare[i];
+            fare[i] = fare[j];
+            fare[j] = temp;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(", ", fare));
+
+Console.WriteLine();
+
+Console.Write("Enter a fare to search :");
+decimal search = decimal.Parse(Console.ReadLine());
+var result = Array.Find(fare, element => element.Equals(search));
+
+if (result != null)
+{
+    Console.WriteLine("Result Found " + result);
+}
+else
+{
+    Console.WriteLine("Result not found");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Maximum and Minimum Values");
+decimal max = fare.Max();
+Console.WriteLine($"Maximum Fare :: {max}");
+
+decimal min = fare.Min();
+Console.WriteLine($"Minimum Fare :: {min}");
+
+Console.WriteLine();
+
+Console.WriteLine("Duplicate Values ");
+
+for (int i = 0; i < fare.Length; i++)
+{
+    int count = 1;
+    for (int j = i+1; j < fare.Length; j++)
+    {
+        if (fare[i] == fare[j])
+        {
+            count++;
+        }
+    }
+    if(count > 1)
+    {
+        Console.WriteLine(fare[i]);
+    }
+}
