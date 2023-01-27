dd314fa9d8b7b5ed5da5d4cecbd147a59cbc8b1d
nandanaHari <117264004+nandanaHari@users.noreply.github.com>
2023-01-27 13:15:15 +0530

    Add files via upload

---
+++ Question5/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Enter the length : ");
+int length = int.Parse(Console.ReadLine());
+
+decimal[] fare = new decimal[length];
+decimal[] sortedFare = new decimal[length * 2];
+
+for (int i = 0; i < fare.Length; i++)
+{
+    Console.WriteLine("Enter Flight Fare :: ");
+    fare[i] = decimal.Parse(Console.ReadLine());
+}
+
+int even = 0;
+int odd = 1;
+
+for (int i = 0; i < fare.Length; i++)
+{
+    if((fare[i] %2) == 0){
+        sortedFare[even] = fare[i];
+        even += 2;
+    }
+    else
+    {
+        sortedFare[odd] = fare[i];
+        odd += 2;
+    }
+}
+
+Console.WriteLine(string.Join(", ", sortedFare));
+
