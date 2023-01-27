ffaa182946091210940afca1f25f058e367fd437
nandanaHari <117264004+nandanaHari@users.noreply.github.com>
2023-01-27 13:22:02 +0530

    Add files via upload

---
+++ Question15/Program.cs
@@ -0,0 +1,48 @@
+﻿int weightDiff;
+
+string[] boardPass = new string[3];
+int[] weight = new int[3];
+int[] weightDIffArray = new int[weight.Length];
+
+for (int i = 0; i < boardPass.Length; i++)
+{
+    Console.WriteLine("enter your name");
+    string name = Console.ReadLine();
+    boardPass[i] = name;
+    Console.WriteLine("weigth");
+    int weightElement = Convert.ToInt32(Console.ReadLine());
+    weight[i] = weightElement;
+
+
+}
+
+Console.WriteLine("\n");
+for (int i = 0; i < boardPass.Length; i++)
+{
+
+    Console.WriteLine("name-{0}", boardPass[i]);
+    Console.WriteLine("weight-{0}kg", weight[i]);
+    Console.WriteLine($"weigth charge {weightFun(weight[i])}$");
+    weightDIffArray[i] = weightFun(weight[i]);
+    Console.WriteLine("\n");
+}
+
+int weightFun(int weight)
+{
+    int weightDiffInFun = (weight - 23);
+    if (weightDiffInFun > 0)
+    {
+        return (20 + (weightDiffInFun * 15));
+    }
+    else
+    {
+        return 20;
+    }
+}
+
+Console.WriteLine("Descending Overweight fare");
+Array.Sort(weightDIffArray);
+for (int i = (weightDIffArray.Length - 1); i >= 0; i--)
+{
+    Console.WriteLine($"{weightDIffArray[i]}$");
+}
---
+++ Question16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+var arr = new string[5, 5]
+{
+ {"1","2","3","4","5"},
+ {"6","7","8","9","10"},
+ {"11","12","13","14","15"},
+ {"16","17","18","19","20"},
+ {"21","22","23","24","25"}
+}; int k = 1;
+while (k <= 10)
+{
+    Console.WriteLine("Enter the seat number:");
+    string seatNum = Console.ReadLine();
+    Console.WriteLine("\nChoose food preference: Veg/Non-Veg");
+    string pref = Console.ReadLine();
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            if (arr[i, j] == seatNum)
+            {
+                arr[i, j] = seatNum + " :" + pref;
+            }
+        }
+    }
+    k++;
+}
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write(arr[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
